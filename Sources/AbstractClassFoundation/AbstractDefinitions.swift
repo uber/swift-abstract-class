88b7edb4d729ecd91e4b75b92d8f7f10bef742a2
Yi Wang <yiw@uber.com>
2019-02-26 11:25:20 -0800

    Add foundation types
    Also added Cartfile to support Carthage

---
+++ Sources/AbstractClassFoundation/AbstractDefinitions.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright (c) 2018. Uber Technologies
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR COITIONS OF ANY KI, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+/// A protocol declaring the conforming class as an abstract class.
+/// This means the conforming class cannot be directly instantiated,
+/// and it may contain abstract methods that subclasses must provide
+/// implementations for.
+///
+/// - SeeAlso: abstractMethod()
+public protocol AbstractClass: AnyObject {}
+
+/// Using this definition declares the enclosing method as an abstract
+/// method, where subclasses of the enclosing class must override and
+/// provide a concrete implementation for.
+public func abstractMethod(_ functionName: String = #function) -> Never {
+    fatalError("Abstract method \(functionName) is not implemented.")
+}
